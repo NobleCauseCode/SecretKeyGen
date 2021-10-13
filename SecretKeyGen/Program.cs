a4b053c7ac14f85e53f2c3f454dbfc2211a68c40
jeff.noble <savijcoder@outlook.com>
2021-10-12 21:26:58 -0400

    * Initial Commit

---
+++ SecretKeyGen/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SecretKeyGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomNumberGenerator = System.Security.Cryptography.RandomNumberGenerator.Create();
+            var bytes = new byte[256 / 8];
+            randomNumberGenerator.GetBytes(bytes);
+            Console.WriteLine(Convert.ToBase64String(bytes));
+        }
+    }
+}
